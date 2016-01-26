00f0f60f745a72b8ad524f20437af352406e8cdd
pablo.david.grimaldi <pablo.david.grimaldi@accenture.com>
2016-01-26 15:27:44 -0300

    Add test for range 1 to 3

--- app/controllers/romannumbers.controller.js
+++ app/controllers/romannumbers.controller.js
@@ -1,16 +1,22 @@
 var dojo = angular.module('dojoApp',[]);
 
-
 dojo.controller('RomanNumbersController', function($scope){
-
 	$scope.convertNumberToRoman = function (number) {
+		var romanSymbols = ['I', 'V', 'X'];
 
-		if (number === 1){
-			 return 'I';
+		if(number >= 1 && number < 4)
+		{
+			var result = "";
+			for (i = 0; i < number; i++) { 
+			    result = result + romanSymbols[0]; 
+			}
+			return result;
 		}
-        else{
+		else if(number === 10){
+			return 'X'
+		}
+		else {
 			return 'V';
 		}
 	}
-
-});
+}); 
--- app/tests/romannumbers.controller.spec.js
+++ app/tests/romannumbers.controller.spec.js
@@ -19,11 +19,24 @@ describe("Roman Nummers controller", function() {
 	}); 
 
     it("Should return I for the number 1", function() {
-
 	   expect(scope.convertNumberToRoman(1)).toBe('I');
 	});
 
 	it("Should return V for the number 5", function() {
 	   expect(scope.convertNumberToRoman(5)).toBe('V');
 	});
-});
+
+	it("Should return II for the number 2", function() {
+	   expect(scope.convertNumberToRoman(2)).toBe('II');
+	});
+
+	it("Should return X for the number 10", function() {
+	   expect(scope.convertNumberToRoman(10)).toBe('X');
+	});
+
+	it("Should return III for the number 3", function() {
+	   expect(scope.convertNumberToRoman(3)).toBe('III');
+	});
+
+
+}); 
